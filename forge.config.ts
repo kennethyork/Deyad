9f947c1ab1606c534a1f83648055737461a65c55
kenny <kenneth@localhost>
2026-03-07 20:41:34 -0500

    force lowercase executableName for packaging

--- forge.config.ts
+++ forge.config.ts
@@ -10,6 +10,7 @@ import { FuseV1Options, FuseVersion } from '@electron/fuses';
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    executableName: 'deyad',      // lowercase to satisfy installer expectations
   },
   rebuildConfig: {},
   makers: [
